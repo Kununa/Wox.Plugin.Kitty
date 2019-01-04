24b865bb2297b34f26974e5df28102abf81fe9a4
Tillmann Schindler <tillmann_s@hotmail.com>
2019-01-04 18:29:13 +0100

    Added configuration options

--- Wox.Plugin.Kitty/Main.cs
+++ Wox.Plugin.Kitty/Main.cs
@@ -2,24 +2,32 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace Wox.Plugin.Kitty
 {
     public class Main : IPlugin
     {
-        private string kittyFolderPath = "D:\\Apps\\kitty\\";
+        private string kittyFolderPath;
         private string kittyPath;
 
         public void Init(PluginInitContext context)
         {
-            kittyPath = kittyFolderPath + "kitty.exe";
+            string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string confFileName = Path.Combine(assemblyFolder, "path.conf");
+            kittyFolderPath = File.ReadAllText(confFileName).Replace(System.Environment.NewLine, "") + "\\";
+            kittyPath = FindKittyExe();
         }
 
         public List<Result> Query(Query query)
         {
             List<Result> results = new List<Result>();
+            if (kittyFolderPath == "" || kittyPath == "")
+                return results;
             foreach (string session in KittySessions())
             {
+                if (session == "Default%20Settings")
+                    continue;
                 results.Add(new Result()
                 {
                     Title = session,
@@ -38,6 +46,17 @@ namespace Wox.Plugin.Kitty
         private string[] KittySessions()
         {
             return (new List<string>(Directory.GetFiles(kittyFolderPath + "Sessions"))).Select(x => x.Split('\\').Last()).ToArray();
+        }
+
+        private string FindKittyExe()
+        {
+            string exe = kittyFolderPath + "kitty.exe";
+            if (File.Exists(exe))
+                return exe;
+            exe = kittyFolderPath + "kitty_portable.exe";
+            if (File.Exists(exe))
+                return exe;
+            return "";
         }
     }
 }
