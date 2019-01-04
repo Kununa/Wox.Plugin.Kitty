6c00d7cdf2ba9c754ffec353b56384b7cfae085a
Tillmann Schindler <tillmann_s@hotmail.com>
2019-01-04 18:07:47 +0100

    Add project files.

---
+++ Wox.Plugin.Kitty/Main.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Wox.Plugin.Kitty
+{
+    public class Main : IPlugin
+    {
+        private string kittyFolderPath = "D:\\Apps\\kitty\\";
+        private string kittyPath;
+
+        public void Init(PluginInitContext context)
+        {
+            kittyPath = kittyFolderPath + "kitty.exe";
+        }
+
+        public List<Result> Query(Query query)
+        {
+            List<Result> results = new List<Result>();
+            foreach (string session in KittySessions())
+            {
+                results.Add(new Result()
+                {
+                    Title = session,
+                    IcoPath = "kitty.png",  //relative path to your plugin directory
+                    Action = e =>
+                    {
+                        Process.Start(kittyPath, "-load " + session);
+                        // after user select the item
+                        return true;
+                    }
+                });
+            }
+            return results;
+        }
+
+        private string[] KittySessions()
+        {
+            return (new List<string>(Directory.GetFiles(kittyFolderPath + "Sessions"))).Select(x => x.Split('\\').Last()).ToArray();
+        }
+    }
+}
